4ecf3f4921a5b77e187044da5d408837281c9ea6
Brett Bloxom <brettbloxom@gmail.com>
2020-07-21 14:00:46 -0700

    Persisting cart with local storage

--- src/context/StoreContext.js
+++ src/context/StoreContext.js
@@ -16,20 +16,37 @@ const defaultValues = {
 export const StoreContext = createContext(defaultValues)
 
 export const StoreProvider = ({ children }) => {
-  const [checkoutId, setCheckoutId] = useState({})
-
-  const initializeCheckout = async () => {
-    try {
-      const newCheckout = await client.checkout.create()
-      setCheckoutId(newCheckout.id)
-    } catch (e) {
-      console.error(e)
-    }
-  }
+  const [checkout, setCheckout] = useState({})
 
   useEffect(() => {
     initializeCheckout()
   }, [])
+
+  const initializeCheckout = async () => {
+    try {
+      // Check if it's a browser
+      const isBrowser = typeof window !== 'undefined'
+
+      // Check if id exists
+      const currentCheckoutId = isBrowser ? localStorage.getItem('checkout_id') : null
+
+      let newCheckout = null
+
+      if (currentCheckoutId) {
+        // If id exists, fetch checkout from Shopify
+        newCheckout = await client.checkout.fetch(currentCheckoutId)
+      } else {
+        // If id does not, create new checkout
+        newCheckout = await client.checkout.create()
+        if (isBrowser) {
+          localStorage.setItem('checkout_id', newCheckout.id)
+        }
+      }
+
+      // Set checkout to state
+      setCheckout(newCheckout)
+    } catch (e) {}
+  }
 
   const addProductToCart = async variantId => {
     try {
@@ -39,7 +56,7 @@ export const StoreProvider = ({ children }) => {
           quantity: 1,
         },
       ]
-      const addItems = await client.checkout.addLineItems(checkoutId, lineItems)
+      const addItems = await client.checkout.addLineItems(checkout.id, lineItems)
       // Buy Now Button Code
       // window.open(addItems.webUrl, "_blank")
       console.log(addItems.webUrl)
